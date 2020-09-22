c9aceeebaa32933a71ac4e911d8fddef62038218
Janita <bgj369@myy.haaga-helia.fi>
2020-09-22 15:57:27 +0300

    Muokkaus, lisäys ja poisto ominaisuus lisätty

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,6 +28,36 @@ public class BookController {
 		return "firstpage";
 
 	}
+	
+	//tyhjän kirjalomakkeen muodostaminen
+	@RequestMapping(value = "/addbook", method = RequestMethod.GET)
+	public String getNewBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "secondpageadd";
+	}
+	
+	//lomakkeella syötettyjen tietojen vastaanotto ja tallennus
+	@RequestMapping(value = "/savebook", method = RequestMethod.POST)
+	public String saveBook(@ModelAttribute Book book) {
+		bookRepository.save(book);
+		return "redirect:/index";
+	}
+	
+	//Kirjan poisto
+	@RequestMapping(value="/deletebook/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../index";
+	}
+	
+	//Kirjan muokkaaminen
+	@RequestMapping(value= "/editbook/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		Book book = bookRepository.findById(bookId).get();
+		model.addAttribute("book", book);
+		return "edit";
+	}
+	
 	
 	
 

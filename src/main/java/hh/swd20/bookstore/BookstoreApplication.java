a7755e49fcf170824b56f565c9d5f957c1f9db1a
Janita <bgj369@myy.haaga-helia.fi>
2020-09-22 13:56:34 +0300

    Repository, commandlinerunner ja kirjalista lisätty

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -2,12 +2,38 @@ package hh.swd20.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+		return (args) -> {
+			log.info("Test data");
+			Book book1 = new Book("Harry Potter", "JK.Rowling", 2007, 1034728942, 26.95);
+			Book book2 = new Book("Da Vinci Code", "Dan Brown", 2003, 1034728943, 37.95);
+			bookRepository.save(book1);
+			bookRepository.save(book2);
+
+			log.info("Fetch books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
 	}
 
 }
--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -1,5 +1,10 @@
 package hh.swd20.bookstore.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,11 +13,20 @@ import hh.swd20.bookstore.domain.*;
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	BookRepository bookRepository;
+
+	
+	//kirja listaus
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String startingPage(Model model) {
-		model.addAttribute("book", new Book());
+		List<Book> booklist = (List<Book>) bookRepository.findAll();
+		model.addAttribute("books", booklist);
 		return "firstpage";
-		
+
 	}
+	
+	
 
 }

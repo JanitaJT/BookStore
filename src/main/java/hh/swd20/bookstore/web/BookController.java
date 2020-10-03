3527b9996cade82f043990c85f2d1ba8ab1b36b9
Janita <bgj369@myy.haaga-helia.fi>
2020-10-03 15:30:24 +0300

    Rest ominaisuudet lisätty

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -1,18 +1,22 @@
 package hh.swd20.bookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.bookstore.domain.*;
 
+@CrossOrigin
 @Controller
 public class BookController {
 	
@@ -29,6 +33,24 @@ public class BookController {
 		model.addAttribute("books", booklist);
 		return "firstpage";
 
+	}
+	
+	//REST to get all books
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	//REST finding by id
+	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){
+		return bookRepository.findById(bookId);
+	}
+	
+	//Rest save new book
+	@RequestMapping(value="/books", method = RequestMethod.POST)
+	public @ResponseBody Book saveBookRest(@RequestBody Book book) {
+		return bookRepository.save(book);
 	}
 	
 	//tyhjän kirjalomakkeen muodostaminen
